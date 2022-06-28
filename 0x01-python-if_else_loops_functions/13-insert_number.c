72f1e97b5bde38f0b860c119404efc94e06be9c2
alli-cpp <alli-cpp@github.com>
2022-06-28 22:02:08 +0000

    task 13 insert nuber c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,43 @@
+nclude "lists.h"
+/**
+ * insert_node - a function that inserts a node
+ * @head: the head of the node
+ * @number: the number value
+ * Return: head
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node, *temp;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+		return (*head);
+	}
+	temp = *head;
+	if (temp->n > node->n)
+	{
+		node->next = temp;
+		*head = node;
+		return (*head);
+	}
+	while (temp->next != NULL)
+	{
+		if (node->n < temp->next->n)
+		{
+			node->next = temp->next;
+			temp->next = node;
+			return (*head);
+		}
+		temp =  temp->next;
+	}
+	temp->next = node;
+	node->next = NULL;
+	return (*head);
+}
