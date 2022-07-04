daed3fa4784e595af2a0c529202ad12d63551dbc
alli-cpp <alli-cpp@github.com>
2022-07-04 18:32:40 +0000

    task 2 replace in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-if idx < 0:
-return (my_list)
-lengh = len(my_list)
-if idx > length - 1:
-return (my_list)
-my_list[idx] = element
-return (my_list)
+    i = 0
+    if idx < 0:
+        return my_list
+    for j in my_list:
+        i += 1
+    if idx > i - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
