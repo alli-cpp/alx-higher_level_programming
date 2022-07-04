4949305ca447718cbf91729abd78f07367b3571c
alli-cpp <alli-cpp@github.com>
2022-07-04 18:19:35 +0000

    task 2 replace in list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    i = 0
+    if idx < 0:
+        return my_list
+    for j in my_list:
+        i += 1
+    if idx > i - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return 
