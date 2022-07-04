9eb73909489a2cbe6ece42c10ff7c6a1146e5f86
alli-cpp <alli-cpp@github.com>
2022-07-04 19:13:37 +0000

    task 13 linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,77 @@
 #include "lists.h"
+
 /**
- * is_palindrome - prints all elements of a listint_t list
- * @head: pointer to head of list
- * Return: number of nodes
+ * len - a function that returns the lenght of a list
+ * @head: the head of the list
+ * Return: the length
  */
 
-int is_palindrome(listint_t **head)
+int len(listint_t **head)
 {
-	listint_t *temp = *head, *temp2 = *head;
-	int array[5000];
-	int len, j, i;
+	listint_t *temp = *head;
+	int count = 0;
 
-	if (!head)
-		return (0);
-	if (!*head || ((*head)->next == NULL))
-		return (1);
-
-	for (len = 0; temp2->next != NULL; len++)
+	while (temp != NULL)
 	{
-		temp2 = temp2->next;
+		temp = temp->next;
+		count++;
 	}
-	for (i = 0; i <= len; i++)
+	return (count);
+}
+/**
+ * channel -  a function that channels all the data of alist
+ * @head: the head of the list
+ * Return: an array of the data from the list
+ */
+int *channel(listint_t **head)
+{
+	int *int_arr, i = 1;
+	listint_t *temp = *head;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (NULL);
+	int_arr = malloc(sizeof(int) * (len(head) + 2));
+	if (int_arr == NULL)
+		return (NULL);
+	while (temp != NULL)
 	{
-		array[i] = temp->n;
+		int_arr[i] = temp->n;
+		i++;
 		temp = temp->next;
 	}
-
-	for (j = 0, len; j < len; j++, len--)
+	int_arr[0] = i - 1;
+	int_arr[i] = '\0';
+	return (int_arr);
+}
+/**
+ * pali - a function that checks for palindrome
+ * @arr: an array to be checked
+ * @fst: index of the first number
+ * @lst: index of the last number
+ * Return: 0 or 1
+ */
+int pali(int *arr, int fst, int lst)
+{
+	if (arr[fst] == arr[lst])
 	{
-		if (array[j] != array[len])
-		{
-			return (0);
-		}
+		if (fst + 1 == lst - 1 || lst == fst + 1)
+			return (1);
+		return (pali(arr, fst + 1, lst - 1));
 	}
-	return (1);
+	return (0);
+}
+/**
+ * is_palindrome - a function that checks a list
+ * @head: the head
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+	int *arr = channel(head), i;
+
+	if (arr == NULL)
+		return (1);
+	i = pali(arr, 1, arr[0]);
+	free(arr);
+	return (i);
 }
