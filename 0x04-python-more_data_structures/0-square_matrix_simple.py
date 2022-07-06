a8c92e81470f1d877bf81c35b0edc5f046ca374a
alli-cpp <alli-cpp@github.com>
2022-07-06 22:40:47 +0000

    task 0 saure matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = []
+    new_matrix = []
     for i in matrix:
-        new.append(list(map(lambda x: x**2, i)))
-    return new
+        new_i = []
+        new_matrix.append(new_i)
+        for j in i:
+            new_i.append(j ** 2)
+    return new_matrix
