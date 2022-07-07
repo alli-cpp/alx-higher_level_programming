d31606fbfa63a72516b2767b1f3a8d2224248a51
alli-cpp <alli-cpp@github.com>
2022-07-07 21:59:48 +0000

    task 12 roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    num, i = 0, 0
-    let = roman_string
-    if not let or type(let) != str:
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
+    total = 0
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for j in let:
-        if val.get(j, 0) == 0:
-            return 0
-    while i < len(let):
-        if num == 0:
-            num = val[let[i]]
-            i += 1
-            continue
-        elif i <= 2 and val[let[i]] == val[let[i - 1]]:
-            if val[let[i]] and val[let[i - 2]] in (10, 100, 1000):
-                num *= val[let[i]]
-            else:
-                num += val[let[i]]
-            i += 1
-            continue
-        elif val[let[i]] > val[let[i - 1]]:
-            num = val[let[i]] - num
-            i += 1
-            continue
-        elif val[let[i]] <= val[let[i - 1]]:
-            num += val[let[i]]
-            i += 1
-            continue
-    return num
+    if len(roman_string) == 1:
+        return romans.get(roman_string)
+    for i in range(len(roman_string)):
+        if i + 1 == len(roman_string):
+            total += romans.get(roman_string[i])
+        elif romans.get(roman_string[i]) >= romans.get(roman_string[i + 1]):
+            total += romans.get(roman_string[i])
+        else:
+            total -= romans.get(roman_string[i])
+    return total
