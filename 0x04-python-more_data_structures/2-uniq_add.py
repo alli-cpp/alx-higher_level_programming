176ab6e7680cda5be8415b728e20c72dc2f99243
alli-cpp <alli-cpp@github.com>
2022-07-07 21:39:26 +0000

    task 2 uniq add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    my_list.sort()
-    prev = None
-    res = 0
-    for i in my_list:
-        if i != prev:
-            prev = i
-            res += prev
-    return 
+uniq_add = __import__('2-uniq_add').uniq_add
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
