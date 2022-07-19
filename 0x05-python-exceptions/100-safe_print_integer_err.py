a76bd6393a075cd9cdc54e15821e1c04c33a5435
alli-cpp <alli-cpp@github.com>
2022-07-19 20:15:06 +0000

    task 7 safe print integer err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
 
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as er:
-        print("Exception: {}".format(er), file=sys.stderr)
-        return False
-    return True
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return (False)
+    else:
+        return (True)
