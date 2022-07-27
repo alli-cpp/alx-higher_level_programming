08c825961d7a49084d18627a5ec40ddecbaca389
alli-cpp <alli-cpp@github.com>
2022-07-27 19:54:10 +0000

    task 1 sec rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,6 +2,7 @@
 """ empty class Rectangle that defines a rectangle
 """
 
+
 class Rectangle:
     """ class rectangle"""
     def __init__(self, width=0, height=0):
@@ -39,4 +40,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = 
+        self.__height = value
