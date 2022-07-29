e5ba409f25e031f1b1bfdc427632538ad4bab880
alli-cpp <alli-cpp@github.com>
2022-07-29 19:05:01 +0000

    task 0 add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Integers addition"""
+
+
+def add_integer(a, b=98):
+    """Adds two numbers
+    Args:
+        a(int) - first number
+        b(int) - second number
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
