d459607a84984532bb55553742e8e5e1bd2da94d
alli-cpp <alli-cpp@github.com>
2022-10-14 20:56:59 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""
+Desc: This module contains a script to  lists all
+states from the database hbtn_0e_0_usa
+"""
+
+from sys import argv
 import MySQLdb
-import sys
-
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states_info = cur.fetchall()
+
+    for state in states_info:
+        print(state)
