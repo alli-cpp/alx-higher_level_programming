3be3fc54ded39bcc81f24ed22b2e40cd9d7ad08c
alli-cpp <alli-cpp@github.com>
2022-11-21 03:38:13 -0800

    task1 fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""class MyList that inherits from list
-"""
+"""Class MyList that inherits from list"""
 
 
 class MyList(list):
-    """inherits from list"""
+    """prints the list sorted"""
+
     def print_sorted(self):
-        """prints the list, but sorted
-        (ascending sort)
-        """
-        print(sorted(self))
+        """print the list sorted"""
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
