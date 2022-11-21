a646341f0a8be59155571794b431b510605129f9
alli-cpp <alli-cpp@github.com>
2022-11-21 12:39:16 +0000

    fixed pep8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""add an item"""
-from sys import argv
-import json
-import os
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""
+Add all arguments to a Python list and save them to a file
+"""
 
+from sys import argv as av
+from os.path import exists
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
-if os.path.isfile("add_item.json"):
-    my_list = load_from_json_file("add_item.json")
-else:
-    my_list = []
+name = "add_item.json"
 
-for arg in range(1, len(argv)):
-    my_list.append(argv[arg])
-save_to_json_file(my_list, "add_item.json")
+av.remove(av[0])
+pre_loaded = []
+
+if exists('add_item.json'):
+    pre_loaded = load(name)
+
+pre_loaded += av
+
+save(pre_loaded, name)
