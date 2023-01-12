0c885d406b7b00538da8a27097bb1c799e369218
alli-cpp <alli-cpp@github.com>
2023-01-12 12:22:05 -0800

    task fix

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+cities from the database hbtn_0e_4_usa.
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name,
+            states.name FROM cities
+            INNER JOIN states
+            ON cities.state_id = states.id
+            ORDER BY cities.id ASC""")
+    states_info = cur.fetchall()
+
+    for state in states_info:
+        print(state)
